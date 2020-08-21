6423138478531dc625669661a8603e9f180f5565
bianca <biancapalmaria@gmail.com>
2020-08-21 05:00:56 +0200

    Added lesson java console log 1,2,3

---
+++ java02_String/java02_String.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java02_String;
+
+/**
+ *
+ * @author Alex
+ */
+public class java02_String {
+     public static void main(String[] args){
+    String salutacio = "Hola!!!"; //hem creat una variable de tipus "string" 
+    System.out.println(salutacio);
+     }
+    
+}
