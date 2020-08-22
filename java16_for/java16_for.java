6fd8f021549d19fcc77480caba3b7ba505ff7c10
bianca <biancapalmaria@gmail.com>
2020-08-22 04:19:52 +0200

    Added exercises with for loop

---
+++ java16_for/java16_for.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java16_for;
+
+/**
+ *
+ * @author Alex
+ */
+public class java16_for {
+  public static void main(String[] args) {
+   
+      for (int i = 0; i < 21; i++) {
+  
+          System.out.println(i);
+
+      }
+      
+   
+  }
+}
---
+++ java17_for/java17_for.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java17_for;
+
+/**
+ *
+ * @author Alex
+ */
+public class java17_for {
+     
+   
+   public static void main(String[] args) {
+   
+      for (int i = 20; i >=0; i--) {
+  
+          System.out.println(i);
+
+      }
+      
+   
+  }
+}
