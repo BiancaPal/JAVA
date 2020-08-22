816011cf6009dbc4ec3ca14f083772fc741a1e12
bianca <biancapalmaria@gmail.com>
2020-08-22 04:11:20 +0200

    Added exercises with do while

---
+++ java15_while/java15_while.java
@@ -0,0 +1,25 @@
+package java15_while;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author Alex
+ */
+public class java15_while {
+    public static void main(String[] args) {
+   
+       int i = 20;
+       do {
+             System.out.println(i);
+             i--;
+        }
+       while (i >= 0);
+   
+  }
+}
