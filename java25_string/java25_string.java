c7aedd59b74d199d827c574e0d773395298fb9dc
bianca <biancapalmaria@gmail.com>
2020-08-24 12:13:19 +0200

    Added exercise lesson with string method replace()

---
+++ java25_string/java25_string.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java25_string;
+
+/**
+ *
+ * @author Alex
+ */
+public class java25_string {
+     public static void main(String[] args) {  
+	String str = "Substituir una paraula";
+	
+        System.out.println(str.replace("una", "la"));
+	
+   }  
+}
