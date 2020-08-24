cdf52a53f19c8a552cfe518de14aff3716dfb4a2
bianca <biancapalmaria@gmail.com>
2020-08-24 23:45:27 +0200

    Added exercise lesson with alternation between string method toUpperCase() and toLowerCase()

--- java26_string/java26_string.java
+++ java26_string/java26_string.java
@@ -9,11 +9,37 @@ package java26_string;
  *
  * @author Alex
  */
+
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class java26_string {
     public static void main(String[] args) {  
-	String str = "Substituir una paraula";
-	
-        System.out.println(str.toUpperCase());
-	
+        
+        Scanner lector = new Scanner(System.in); 
+        
+        System.out.println("Please enter String array"); 
+        
+       
+       //For using to get the input string that was skipped of the Scanner object.(Use it when scan a string after scanning different type of  variables.)
+       String s = lector.nextLine();
+
+       //For print String, Double and Integer variable
+    
+       System.out.println("String: " + s);
+       
+        
+        String[] strSplit= s.split(" "); // making an array out of the split method
+        
+        for (int i = 0; i < strSplit.length; i++){
+           
+            if (i % 2 == 0) {
+              System.out.print(" "+strSplit[i].toUpperCase());
+            } else {
+              System.out.print(" "+strSplit[i].toLowerCase());
+            } 
+            
+        }
+      
    }  
 }
---
+++ java27_string/java27_string.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java27_string;
+
+/**
+ *
+ * @author Alex
+ */
+public class java27_string {
+   public static void main(String[] args) {  
+	String str = "Substituir una paraula";
+	
+        System.out.println(str.toUpperCase());
+	
+   }     
+}
