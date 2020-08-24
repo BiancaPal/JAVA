3caef832af348daaf2f861e65f6b1d31e3aa7bea
bianca <biancapalmaria@gmail.com>
2020-08-25 00:10:27 +0200

    Added exercise lesson with charAt() string method

--- java27_string/java27_string.java
+++ java27_string/java27_string.java
@@ -9,11 +9,27 @@ package java27_string;
  *
  * @author Alex
  */
+
+import java.util.Scanner; 
+
 public class java27_string {
-   public static void main(String[] args) {  
-	String str = "Substituir una paraula";
-	
-        System.out.println(str.toUpperCase());
-	
-   }     
+  public static void main(String[] args) {  
+        
+        Scanner lector = new Scanner(System.in); 
+        
+        System.out.println("Please enter String array"); 
+        
+       
+       //For using to get the input string that was skipped of the Scanner object.(Use it when scan a string after scanning different type of  variables.)
+       String s = lector.nextLine();
+     
+        for (int i = 0; i < s.length(); i++){
+            if (i % 2 == 0){
+                System.out.print(s.toUpperCase().charAt(i));
+            } else{
+                System.out.print(s.toLowerCase().charAt(i));
+            }
+        }
+      
+   }   
 }
