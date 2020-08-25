58d3bc149ccf3eb71f8a9032382c67449fdf226b
bianca <biancapalmaria@gmail.com>
2020-08-26 01:17:09 +0200

    Added exercise with set() GregorianCalendar method

---
+++ java37_GregorianCalendar/java37_GregorianCalendar.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java37_GregorianCalendar;
+
+import java.util.GregorianCalendar;
+/**
+ *
+ * @author Alex
+ */
+public class java37_GregorianCalendar {
+     public static void main(String args[]){  
+       
+        
+       GregorianCalendar calendari;
+       
+       calendari = new GregorianCalendar();
+       
+       calendari.set(1973, 6, 23);
+      
+       System.out.print(calendari.getTime() );
+      
+
+          
+          
+          
+        
+    }
+}
