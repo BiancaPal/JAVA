21b69178c03fa73959205b433f4275e81aea33a9
bianca <biancapalmaria@gmail.com>
2020-08-26 05:00:44 +0200

    Added exercise with alphabet

---
+++ java43_array/java43_array.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java43_array;
+
+/**
+ *
+ * @author Alex
+ */
+
+import java.util.Arrays;
+
+public class java43_array {
+     public static void main(String[] args)
+    {
+     
+           
+         String [] alfabet = {"A", "B", "C", "D", "E", "F", "G", "H", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "W", "X", "Y", "Z"};
+         
+         for (String alfa: alfabet){
+             System.out.print(alfa);
+         }
+         
+         System.out.print("\n"+Arrays.toString(alfabet));
+         
+         
+         
+           
+           
+           
+            
+    }
+}
