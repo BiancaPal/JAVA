6be1a792faf2fb87c5942023dd7d06916ff9e9ec
bianca <biancapalmaria@gmail.com>
2020-08-26 04:53:45 +0200

    Added exercise lesson with alphabet toLowerCase() method

---
+++ java42_array/java42_array.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java42_array;
+
+/**
+ *
+ * @author Alex
+ */
+
+import java.util.Arrays;
+
+public class java42_array {
+    
+     public static void main(String[] args)
+    {
+     
+           
+         String [] alfabet = {"A", "B", "C", "D", "E", "F", "G", "H", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "W", "X", "Y", "Z"};
+         
+         for (String alfa: alfabet){
+             System.out.println(alfa.toLowerCase());
+         }
+         
+         
+           
+           
+           
+            
+    }
+}
