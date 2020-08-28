6e8a5708c0a99c63ea84e5bbd437c880b8723f54
bianca <biancapalmaria@gmail.com>
2020-08-28 11:18:34 +0200

    Added random dice result into a for and store it into an ArrayList

--- java58_arrayList/java58_arrayList.java
+++ java58_arrayList/java58_arrayList.java
@@ -9,6 +9,28 @@ package java58_arrayList;
  *
  * @author Alex
  */
+import java.lang.Math;
+import java.util.ArrayList;
+
 public class java58_arrayList {
-    
+     public static void main(String[] args) {
+         
+         ArrayList<Double> numeros = new ArrayList<Double>();
+         
+         int max = 6;
+         int min = 1;
+         int range = max - min +1;
+         
+         for (int i=1; i<=max; i++){
+             numeros.add(Math.random()*range);
+         }
+         
+         for (double numero:numeros){
+             int numeroInt = (int)numero;
+             System.out.print(" "+numeroInt);
+         }
+         
+         
+         
+     }
 }
