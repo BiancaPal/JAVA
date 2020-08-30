3b66a96fef56a52467ff6ff3539a6cf8b3b38b02
bianca <biancapalmaria@gmail.com>
2020-08-31 01:40:12 +0200

    Added exercise with private and getter caller

---
+++ java66_classe/java66_classe.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java66_classe;
+
+/**
+ *
+ * @author Alex
+ */
+public class java66_classe {
+    
+    private String nom;
+    private Double nota;
+    
+    public Double getNota(){
+        return nota;
+    }
+    
+    public String getNom(){
+        return nom;
+    }
+    
+    
+}
